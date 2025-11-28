8771338a9f30e7727362c08d4698a155f33c592b
Clarnc <claus909808@gmail.com>
2025-11-28 14:14:15 +0100

    feat(header): add basic header component

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My Simple Header</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
+
+export default Header;
